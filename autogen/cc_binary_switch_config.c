99c79314ac51fb346b4b14c0bc7386aa22b09584
spatino16 <spatino@tridentiot>
2024-08-20 02:07:32 -0500

    Added SSv5 Files US_freq and debug print

---
+++ autogen/cc_binary_switch_config.c
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * Command Class Binary Switch Configuration
+ * @remarks This file is auto generated
+ * @copyright 2022 Silicon Laboratories Inc.
+ */
+#include <cc_binary_switch_config.h>
+#include <CC_BinarySwitch.h>
+#include <ZW_typedefs.h>
+#include <SizeOf.h>
+#include <Assert.h>
+
+static cc_binary_switch_t binary_switches[] = {
+  {
+    .endpoint = 0,
+    .default_duration = CC_BINARY_SWITCH_CONFIG_DEFAULT_DURATION,
+    .callback = cc_binary_switch_handler
+  },
+};
+
+STATIC_ASSERT((sizeof_array(binary_switches) > 0), STATIC_ASSERT_FAILED_size_mismatch);
+
+cc_binary_switch_t * cc_binary_switch_get_config(void)
+{
+  return binary_switches;
+}
+
+uint8_t cc_binary_switch_get_config_length(void)
+{
+  return sizeof_array(binary_switches);
+}
+
+ZAF_CC_REGISTER_CONFIG(COMMAND_CLASS_SWITCH_BINARY, &binary_switches[0], 0);
